62088f8dd6852978e6017a3184b07a650846e4fa
majestice_coder <142192726+Kapil9711@users.noreply.github.com>
2024-05-03 12:43:59 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 function firstNonRepeatedChar(str) {
 	// Write your code here
-	const l  = str.length;
+	const l  = str.trim().length;
 	for(let i = 0 ; i < l: i++){
 		const char = str[i];
 		if(str.indexOf(char) === i && str.lastIndexOf(char) === i) return char;
